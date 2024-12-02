09cb268a30daeecd727275ee18ffcf40775dc556
casper-pulit <pulitcasper@gmail.com>
2024-12-02 17:54:18 +0000

    day 2 done

--- day2/main.go
+++ day2/main.go
@@ -9,9 +9,6 @@ import (
 	"strings"
 )
 
-var start int
-var prev int
-var desc bool
 var safe []bool
 var safe_count int
 var diff int
@@ -29,85 +26,121 @@ func countTrue(slice []bool) int {
 	return count
 }
 
-func main() {
+func readFile(path string) (*bufio.Scanner, error) {
+	file, err := os.Open(path)
 
-	file, err := os.Open(input_path)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	return scanner, err
+}
+
+func isSafe(s []string) (bool, error) {
+	var prev int
+	var desc bool
+
+	for i := 0; i < len(s); i++ {
+		val, err := strconv.Atoi(s[i])
+
+		if err != nil {
+			return false, err
+		}
+		// first iter
+		if i == 0 {
+			// store value as previous
+			prev = val
+		} else {
+
+			// checks whether ascending descending or neither
+			if i == 1 {
+				if val < prev {
+					desc = true
+				} else if val > prev {
+					desc = false
+				} else {
+					return false, nil
+				}
+			}
+
+			if desc {
+				diff = prev - val
+			}
+
+			if !desc {
+				diff = val - prev
+			}
+
+			prev = val
+
+			if diff < 1 || diff > 3 {
+				return false, nil
+			}
+
+			if i == len(s)-1 {
+				return true, nil
+			}
+
+		}
+	}
+
+	return true, nil
+}
+
+func RemoveIndex(s []string, index int) []string {
+	// this must be cursed
+	ns := make([]string, 0, len(s)-1)
+	if index > 0 {
+		ns = append(ns, s[:index]...)
+	}
+	ns = append(ns, s[index+1:]...)
+
+	return ns
+}
+
+func problemDampner(s []string) bool {
+
+	for i := 0; i < len(s); i++ {
+		ns := RemoveIndex(s, i)
+		is_safe, err := isSafe(ns)
+
+		if err != nil {
+			return false
+		}
+
+		if is_safe {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	sc, err := readFile(input_path)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer file.Close()
-
-	sc := bufio.NewScanner(file)
-
 	for sc.Scan() {
-		// split the strings
+
 		s := strings.Split(sc.Text(), " ")
-		fmt.Println(s)
-		// time.Sleep(2 * time.Second)
-		// iterate over slice
-		for i := 0; i < len(s); i++ {
-			// fmt.Println(s[i])
 
-			val, err := strconv.Atoi(s[i])
+		sc_safe, err := isSafe(s)
 
-			if err != nil {
-				log.Fatal(err)
-			}
-			// first iter
-			if i == 0 {
-				// store value as previous
-				prev = val
-			} else {
+		if err != nil {
+			log.Fatal(err)
+		}
 
-				// checks whether ascending descending or neither
-				if i == 1 {
-					if val < prev {
-						desc = true
-					} else if val > prev {
-						desc = false
-					} else {
-						fmt.Println("not desc or asc, unsafe, breaking")
-						safe = append(safe, false)
-						break
-					}
-				}
-
-				if desc {
-					// fmt.Println("descending")
-					diff = prev - val
-				}
-
-				if !desc {
-					// fmt.Println("ascending")
-					diff = val - prev
-				}
-
-				prev = val
-
-				if diff < 1 || diff > 3 {
-					fmt.Println("not ok")
-					safe = append(safe, false)
-					break
-				} else {
-					// fmt.Println("looks ok")
-				}
-
-				if i == len(s)-1 {
-					safe = append(safe, true)
-					fmt.Println("SUCCESS")
-
-				}
-
-			}
+		if sc_safe {
+			safe = append(safe, sc_safe)
+		} else {
+			safe = append(safe, problemDampner(s))
 		}
 	}
 
-	safe_count = countTrue(safe)
+	fmt.Println("Answer: ", countTrue(safe))
 
-	fmt.Println("Answer: ", safe_count)
 }
-
-// 	The levels are either all increasing or all decreasing.
-// Any two adjacent levels differ by at least one and at most three.
